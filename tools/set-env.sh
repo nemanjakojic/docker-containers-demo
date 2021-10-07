5470e9e3a0f5b758c6bc33610c29ffeed86da42e
nemanjakojic <ubuntu@ubuntu2004.linuxvmimages.local>
2021-10-06 22:21:17 -0400

    updates env parameters

--- tools/set-env.sh
+++ tools/set-env.sh
@@ -2,5 +2,3 @@
 
 export DB_ADMIN_PASS="TestPwd.098"
 export MSSQL_CONTAINER_NAME="mssql-container"
-export DB_DATA_PATH=/home/ubuntu/array-hw-netcore/data
-export DB_LOG_PATH=/home/ubuntu/array-hw-netcore/log
